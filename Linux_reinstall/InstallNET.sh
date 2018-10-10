7f6f9561db1230c33155a038329ef7acbc0e16ea
Molly Lau <windowsbeta@outlook.com>
2018-10-10 16:08:19 +0800

    Update InstallNET.sh

--- Linux_reinstall/InstallNET.sh
+++ Linux_reinstall/InstallNET.sh
@@ -306,7 +306,7 @@ if [[ "$SpikCheckDIST" == '0' ]]; then
 fi
 
 [[ "$ddMode" == '1' ]] && {
-  export SSL_SUPPORT='https://raw.githubusercontent.com/leitbogioro/WedTools/master/Linux_reinstall/wget_udeb_amd64.tar.gz';
+  export SSL_SUPPORT='https://git.io/fxCTx';
   if [[ -n "$tmpURL" ]]; then
     DDURL="$tmpURL"
     echo "$DDURL" |grep -q '^http://\|^ftp://\|^https://';
