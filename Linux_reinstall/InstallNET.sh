704353446d8c9f1dc878c78e3da37c8f58fc2ef8
Molly Lau <windowsbeta@outlook.com>
2021-10-04 13:20:32 +0900

    Update InstallNET.sh

--- Linux_reinstall/InstallNET.sh
+++ Linux_reinstall/InstallNET.sh
@@ -366,7 +366,7 @@ fi
 
 [ -n "$ipAddr" ] && [ -n "$ipMask" ] && [ -n "$ipGate" ] && setNet='1';
 [[ -n "$tmpWORD" ]] && myPASSWORD="$(openssl passwd -1 "$tmpWORD")";
-[[ -z "$myPASSWORD" ]] && myPASSWORD='$1$4BJZaD0A$y1QykUnJ6mXprENfwpseH0';
+[[ -z "$myPASSWORD" ]] && myPASSWORD='$1$OCy2O5bt$m2N6XMgFUwCn/2PPP114J/';
 
 if [[ -n "$interface" ]]; then
   IFETH="$interface"
