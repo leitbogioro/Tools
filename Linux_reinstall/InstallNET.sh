5f540b17e5d543ac4f44d07fd06b9573ea8d51d4
Molly Lau <windowsbeta@outlook.com>
2022-11-28 10:12:28 +0900

    Update InstallNET.sh

--- Linux_reinstall/InstallNET.sh
+++ Linux_reinstall/InstallNET.sh
@@ -994,10 +994,8 @@ if [[ "$ddMode" == '1' ]]; then
     echo "$DDURL" | grep -q '^http://\|^ftp://\|^https://';
     [[ $? -ne '0' ]] && echo 'Please input vaild URL, Only support http://, ftp:// and https:// !' && exit 1;
     # Decompress command selection
-    if [[ $(echo "$DDURL" | grep '.gz') != "" ]] || [[ "$setFileType" == "gz" ]]; then
-        DEC_CMD="gunzip -dc"
-    elif [[ $(echo "$DDURL" | grep '.xz') != "" ]] || [[ "$setFileType" == "xz" ]]; then
-        DEC_CMD="xzcat"
+    DEC_CMD="gunzip -dc"
+    [[ "$setFileType" == "xz" ]] && DEC_CMD="xzcat"
     else
         echo 'Please input vaild URL, Only support gz or xz file!' && exit 1
     fi
