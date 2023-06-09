a0fe211b78606b84671d787bd071151e17b921a8
Molly Lau <windowsbeta@outlook.com>
2023-06-10 01:09:06 +0900

    Update alpineInit.sh

--- Linux_reinstall/Alpine/alpineInit.sh
+++ Linux_reinstall/Alpine/alpineInit.sh
@@ -37,16 +37,14 @@ acpid | default
 crond | default
 seedrng | boot
 
-# Add community mirror
-echo $LinuxMirror/$alpineVer/community >>/etc/apk/repositories
-
 # Reset configurations of repositories
 true >/etc/apk/repositories
 setup-apkrepos $LinuxMirror/$alpineVer/main
 setup-apkcache /var/cache/apk
 
-# Add edge testing to the repositories
+# Add community mirror
 sed -i '$a\'$LinuxMirror'/'$alpineVer'/community' /etc/apk/repositories
+# Add edge testing to the repositories
 sed -i '$a\'$LinuxMirror'/edge/testing' /etc/apk/repositories
 
 # Synchronize time from hardware
