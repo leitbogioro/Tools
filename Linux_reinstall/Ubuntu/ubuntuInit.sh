86df0fdd7126b8b649ae17e3944bf7cc51719dd7
Molly Lau <windowsbeta@outlook.com>
2023-07-01 23:26:15 +0900

    Update ubuntuInit.sh

--- Linux_reinstall/Ubuntu/ubuntuInit.sh
+++ Linux_reinstall/Ubuntu/ubuntuInit.sh
@@ -26,11 +26,13 @@ sshPORT=$(grep "sshPORT" $confFile | awk '{print $2}')
 IPv4=$(grep "IPv4" $confFile | awk '{print $2}')
 MASK=$(grep "MASK" $confFile | awk '{print $2}')
 ipPrefix=$(grep "ipPrefix" $confFile | awk '{print $2}')
+actualIp4Prefix=$(grep "actualIp4Prefix" $confFile | awk '{print $2}')
 GATE=$(grep "GATE" $confFile | awk '{print $2}')
 ipDNS1=$(grep "ipDNS1" $confFile | awk '{print $2}')
 ipDNS2=$(grep "ipDNS2" $confFile | awk '{print $2}')
 ip6Addr=$(grep "ip6Addr" $confFile | awk '{print $2}')
 ip6Mask=$(grep "ip6Mask" $confFile | awk '{print $2}')
+actualIp6Prefix=$(grep "actualIp6Prefix" $confFile | awk '{print $2}')
 ip6Gate=$(grep "ip6Gate" $confFile | awk '{print $2}')
 ip6DNS1=$(grep "ip6DNS1" $confFile | awk '{print $2}')
 ip6DNS2=$(grep "ip6DNS2" $confFile | awk '{print $2}')
@@ -85,11 +87,13 @@ sed -ri 's/targetLinuxMirror/'${targetLinuxMirror}'/g' /mnt/etc/cloud/cloud.cfg.
 sed -ri 's/IPv4/'${IPv4}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/MASK/'${MASK}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/ipPrefix/'${ipPrefix}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
+sed -ri "s/'${IPv4}'\/'${ipPrefix}'/'${IPv4}'\/'${actualIp4Prefix}'/g" /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/GATE/'${GATE}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/ipDNS1/'${ipDNS1}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/ipDNS2/'${ipDNS2}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/ip6Addr/'${ip6Addr}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/ip6Mask/'${ip6Mask}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
+sed -ri "s/'${ip6Addr}'\/'${ip6Mask}'/'${ip6Addr}'\/'${actualIp6Prefix}'/g" /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/ip6Gate/'${ip6Gate}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/ip6DNS1/'${ip6DNS1}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
 sed -ri 's/ip6DNS2/'${ip6DNS2}'/g' /mnt/etc/cloud/cloud.cfg.d/99-fake_cloud.cfg
