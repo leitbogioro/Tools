a6c1853f4c36300bb62d62a4e2b063cb3d058809
Molly Lau <windowsbeta@outlook.com>
2023-07-05 05:06:52 +0900

    Update SetupComplete.bat

--- Linux_reinstall/Windows/SetupComplete.bat
+++ Linux_reinstall/Windows/SetupComplete.bat
@@ -10,6 +10,9 @@ set gateways=GATE
 ::Dns
 set dnsserver1=ipDNS1
 set dnsserver2=ipDNS2
+::Expand system partition
+set systemDisk=%SystemDrive:~0,1%
+for /f "tokens=2" %%a in ('echo list vol ^| diskpart ^| findstr "\<%systemDisk%\>"') do (echo select disk 0 & echo select vol %%a & echo extend) | diskpart
 
 echo;%setmode%|find "on"&&goto:enable||goto:disable
 :enable
@@ -17,4 +20,7 @@ wmic nicconfig where ipenabled=true call enablestatic(%staticip%),(%subnetmask%)
 wmic nicconfig where ipenabled=true call setgateways(%gateways%)
 wmic nicconfig where ipenabled=true call setdnsserversearchorder(%dnsserver1%,%dnsserver2%)
 del %0
+pause
 :disable
+del %0
+pause
