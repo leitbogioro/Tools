4695171484c3503485e88a858244574d4eae3a58
Molly Lau <windowsbeta@outlook.com>
2023-09-15 01:12:20 +0900

    Update InstallNET.sh

--- Linux_reinstall/InstallNET.sh
+++ Linux_reinstall/InstallNET.sh
@@ -2739,6 +2739,7 @@ if [[ -z "$tmpWORD" || "$linux_relese" == 'alpinelinux' || "$targetRelese" == 'U
   tmpWORD='LeitboGi0ro'
   myPASSWORD='$6$qE9Lqgrd0QTOq46i$YMECmKvIw2SeBP4X411I0ZWmtyMsRcBi4Rxu7HYRsqdwqSApi6zjds5UJyM4HrAoBcuLBmjPyLatGydulmCDb0'
 else
+# "-1" is MD5, "-5" is SHA256, "-6" is SHA512. MD5 is no longer secure.
   myPASSWORD=$(openssl passwd -6 ''$tmpWORD'')
 fi
 
