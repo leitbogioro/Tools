1869dcc8ca93a4a578cd1dce55ae2f420225eb54
Molly Lau <windowsbeta@outlook.com>
2023-09-23 17:03:41 +0900

    Update windowsInit.sh

--- Linux_reinstall/Windows/windowsInit.sh
+++ Linux_reinstall/Windows/windowsInit.sh
@@ -54,7 +54,7 @@ apk update
 apk add ca-certificates e2fsprogs fuse gzip hdparm multipath-tools musl ntfs-3g parted util-linux wget xz
 
 # start dd
-wget --no-check-certificate -qO- "$DDURL"  | $DEC_CMD | dd of="$IncDisk"
+wget --no-check-certificate -qO- "$DDURL" | $DEC_CMD | dd of="$IncDisk"
 
 # get valid loop device
 loopDevice=$(echo $(losetup -f))
