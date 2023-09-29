84de4e2f3b939a21ceb8447492b37f9728ccbf4b
Molly Lau <windowsbeta@outlook.com>
2023-09-29 23:43:41 +0900

    Update ubuntuInit.sh

--- Linux_reinstall/Ubuntu/ubuntuInit.sh
+++ Linux_reinstall/Ubuntu/ubuntuInit.sh
@@ -67,7 +67,7 @@ apk update
 apk add ca-certificates e2fsprogs hdparm multipath-tools parted util-linux wget xz
 
 # start dd
-wget --no-check-certificate --report-speed=bits --tries=0 -O- "$DDURL" | $DEC_CMD | dd of="$IncDisk" status=progress
+wget --no-check-certificate --report-speed=bits --tries=0 --timeout=1 --wait=1 -O- "$DDURL" | $DEC_CMD | dd of="$IncDisk" status=progress
 
 # get valid loop device
 loopDevice=$(echo $(losetup -f))
