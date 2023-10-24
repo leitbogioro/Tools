79359d9deaae9466f43d151a0252ace170121abe
Molly Lau <windowsbeta@outlook.com>
2023-10-24 20:13:28 +0900

    Update ubuntuInit.sh

--- Linux_reinstall/Ubuntu/ubuntuInit.sh
+++ Linux_reinstall/Ubuntu/ubuntuInit.sh
@@ -2,7 +2,14 @@
 #
 # Alpine Linux use "ash" as the default shell.
 
-exec >/dev/tty0 2>&1
+# Find available tty.
+for ttyItems in "/dev/tty0" "/dev/ttyS0" "/dev/ttyAMA0"; do
+	ttyAttribute=$(stty -F "$ttyItems")
+	[[ -n "$ttyAttribute" && -n $(echo "$ttyAttribute" | grep -io "\-brkint ixoff \-imaxbel") ]] && {
+		break
+	}
+done
+exec >"$ttyItems" 2>&1
 
 # Delete the initial script itself to prevent to be executed in the new system.
 rm -f /etc/local.d/ubuntuConf.start
